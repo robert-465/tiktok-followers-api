b95cf599e192d83f659f87894b541ba224b1556c
robert-465 <melanieroberts1753@gutkai.com>
2025-11-11 06:25:26 -0500

    feat: add tiktok-followers-api-scraper/src/extractors/utils.py

---
+++ tiktok-followers-api-scraper/src/extractors/utils.py
@@ -0,0 +1,9 @@
+thonimport json
+
+def read_json_file(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
+
+def write_json_file(file_path, data):
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=4)
